5ee5a427c0a495ab833acaf31430f8dbfc88557f
Joshua Crum <joshua.crum@gmail.com>
2019-06-22 16:13:03 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -1,4 +1,17 @@
 class CreateArtists < ActiveRecord::Migration
   def change
+    create_table :artists do |t|
   end
+
+    sql = <<-SQL
+    CREATE TABLE IF NOT EXISTS artists (
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    genre TEXT,
+    age INTEGER,
+    hometown TEXT
+    )
+  SQL
+ 
+  ActiveRecord::Base.connection.execute(sql)
 end
